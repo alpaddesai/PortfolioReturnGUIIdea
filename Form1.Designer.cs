a535bdec21cd3a09c95a751cd7fa5b5bc5fa27cf
unknown <alpa.gupta@gmail.com>
2019-05-19 17:15:48 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace PortfolioReturn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MutualFundA = new System.Windows.Forms.Button();
+            this.MutualFundB = new System.Windows.Forms.Button();
+            this.MutualFundC = new System.Windows.Forms.Button();
+            this.MutualFundD = new System.Windows.Forms.Button();
+            this.MutualFundE = new System.Windows.Forms.Button();
+            this.CustomRateofReturn = new System.Windows.Forms.Button();
+            this.DefaultRateofReturn = new System.Windows.Forms.Button();
+            this.MutualFundAInput = new System.Windows.Forms.TextBox();
+            this.MutualFundBInput = new System.Windows.Forms.TextBox();
+            this.MutualFundCInput = new System.Windows.Forms.TextBox();
+            this.MutualFundDInput = new System.Windows.Forms.TextBox();
+            this.MutualFundEInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MutualFundA
+            // 
+            this.MutualFundA.Location = new System.Drawing.Point(30, 302);
+            this.MutualFundA.Name = "MutualFundA";
+            this.MutualFundA.Size = new System.Drawing.Size(107, 29);
+            this.MutualFundA.TabIndex = 0;
+            this.MutualFundA.Text = "Mutual Fund A";
+            this.MutualFundA.UseVisualStyleBackColor = true;
+            this.MutualFundA.Click += new System.EventHandler(this.MutualFundA_Click);
+            // 
+            // MutualFundB
+            // 
+            this.MutualFundB.Location = new System.Drawing.Point(30, 356);
+            this.MutualFundB.Name = "MutualFundB";
+            this.MutualFundB.Size = new System.Drawing.Size(107, 29);
+            this.MutualFundB.TabIndex = 1;
+            this.MutualFundB.Text = "Mutual Fund B";
+            this.MutualFundB.UseVisualStyleBackColor = true;
+            this.MutualFundB.Click += new System.EventHandler(this.MutualFundB_Click);
+            // 
+            // MutualFundC
+            // 
+            this.MutualFundC.Location = new System.Drawing.Point(292, 302);
+            this.MutualFundC.Name = "MutualFundC";
+            this.MutualFundC.Size = new System.Drawing.Size(107, 29);
+            this.MutualFundC.TabIndex = 2;
+            this.MutualFundC.Text = "Mutual Fund C";
+            this.MutualFundC.UseVisualStyleBackColor = true;
+            this.MutualFundC.Click += new System.EventHandler(this.MutualFundC_Click);
+            // 
+            // MutualFundD
+            // 
+            this.MutualFundD.Location = new System.Drawing.Point(292, 356);
+            this.MutualFundD.Name = "MutualFundD";
+            this.MutualFundD.Size = new System.Drawing.Size(107, 29);
+            this.MutualFundD.TabIndex = 3;
+            this.MutualFundD.Text = "Mutual Fund D";
+            this.MutualFundD.UseVisualStyleBackColor = true;
+            this.MutualFundD.Click += new System.EventHandler(this.MutualFundD_Click);
+            // 
+            // MutualFundE
+            // 
+            this.MutualFundE.Location = new System.Drawing.Point(551, 302);
+            this.MutualFundE.Name = "MutualFundE";
+            this.MutualFundE.Size = new System.Drawing.Size(107, 29);
+            this.MutualFundE.TabIndex = 4;
+            this.MutualFundE.Text = "Mutual Fund E";
+            this.MutualFundE.UseVisualStyleBackColor = true;
+            this.MutualFundE.Click += new System.EventHandler(this.MutualFundE_Click);
+            // 
+            // CustomRateofReturn
+            // 
+            this.CustomRateofReturn.Location = new System.Drawing.Point(551, 373);
+            this.CustomRateofReturn.Name = "CustomRateofReturn";
+            this.CustomRateofReturn.Size = new System.Drawing.Size(258, 34);
+            this.CustomRateofReturn.TabIndex = 5;
+            this.CustomRateofReturn.Text = "Generate Custom Portfolio Rate of Return";
+            this.CustomRateofReturn.UseVisualStyleBackColor = true;
+            this.CustomRateofReturn.Click += new System.EventHandler(this.CustomRateofReturn_Click);
+            // 
+            // DefaultRateofReturn
+            // 
+            this.DefaultRateofReturn.Location = new System.Drawing.Point(687, 204);
+            this.DefaultRateofReturn.Name = "DefaultRateofReturn";
+            this.DefaultRateofReturn.Size = new System.Drawing.Size(258, 34);
+            this.DefaultRateofReturn.TabIndex = 6;
+            this.DefaultRateofReturn.Text = "Generate Default Portfolio Rate of Return";
+            this.DefaultRateofReturn.UseVisualStyleBackColor = true;
+            this.DefaultRateofReturn.Click += new System.EventHandler(this.DefaultRateofReturn_Click);
+            // 
+            // MutualFundAInput
+            // 
+            this.MutualFundAInput.Location = new System.Drawing.Point(159, 307);
+            this.MutualFundAInput.Name = "MutualFundAInput";
+            this.MutualFundAInput.Size = new System.Drawing.Size(96, 20);
+            this.MutualFundAInput.TabIndex = 7;
+            this.MutualFundAInput.TextChanged += new System.EventHandler(this.MutualFundAInput_TextChanged);
+            // 
+            // MutualFundBInput
+            // 
+            this.MutualFundBInput.Location = new System.Drawing.Point(159, 361);
+            this.MutualFundBInput.Name = "MutualFundBInput";
+            this.MutualFundBInput.Size = new System.Drawing.Size(96, 20);
+            this.MutualFundBInput.TabIndex = 8;
+            this.MutualFundBInput.TextChanged += new System.EventHandler(this.MutualFundBInput_TextChanged);
+            // 
+            // MutualFundCInput
+            // 
+            this.MutualFundCInput.Location = new System.Drawing.Point(419, 307);
+            this.MutualFundCInput.Name = "MutualFundCInput";
+            this.MutualFundCInput.Size = new System.Drawing.Size(96, 20);
+            this.MutualFundCInput.TabIndex = 9;
+            this.MutualFundCInput.TextChanged += new System.EventHandler(this.MutualFundCInput_TextChanged);
+            // 
+            // MutualFundDInput
+            // 
+            this.MutualFundDInput.Location = new System.Drawing.Point(419, 361);
+            this.MutualFundDInput.Name = "MutualFundDInput";
+            this.MutualFundDInput.Size = new System.Drawing.Size(96, 20);
+            this.MutualFundDInput.TabIndex = 10;
+            this.MutualFundDInput.TextChanged += new System.EventHandler(this.MutualFundDInput_TextChanged);
+            // 
+            // MutualFundEInput
+            // 
+            this.MutualFundEInput.Location = new System.Drawing.Point(687, 307);
+            this.MutualFundEInput.Name = "MutualFundEInput";
+            this.MutualFundEInput.Size = new System.Drawing.Size(96, 20);
+            this.MutualFundEInput.TabIndex = 11;
+            this.MutualFundEInput.TextChanged += new System.EventHandler(this.MutualFundEInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::PortfolioReturn.Properties.Resources.Image;
+            this.ClientSize = new System.Drawing.Size(957, 450);
+            this.Controls.Add(this.MutualFundEInput);
+            this.Controls.Add(this.MutualFundDInput);
+            this.Controls.Add(this.MutualFundCInput);
+            this.Controls.Add(this.MutualFundBInput);
+            this.Controls.Add(this.MutualFundAInput);
+            this.Controls.Add(this.DefaultRateofReturn);
+            this.Controls.Add(this.CustomRateofReturn);
+            this.Controls.Add(this.MutualFundE);
+            this.Controls.Add(this.MutualFundD);
+            this.Controls.Add(this.MutualFundC);
+            this.Controls.Add(this.MutualFundB);
+            this.Controls.Add(this.MutualFundA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MutualFundA;
+        private System.Windows.Forms.Button MutualFundB;
+        private System.Windows.Forms.Button MutualFundC;
+        private System.Windows.Forms.Button MutualFundD;
+        private System.Windows.Forms.Button MutualFundE;
+        private System.Windows.Forms.Button CustomRateofReturn;
+        private System.Windows.Forms.Button DefaultRateofReturn;
+        private System.Windows.Forms.TextBox MutualFundAInput;
+        private System.Windows.Forms.TextBox MutualFundBInput;
+        private System.Windows.Forms.TextBox MutualFundCInput;
+        private System.Windows.Forms.TextBox MutualFundDInput;
+        private System.Windows.Forms.TextBox MutualFundEInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortfolioReturn
+{
+    public partial class Form1 : Form
+    {
+
+
+        double RateofReturnMutualFundA = 0;
+        double RateofReturnMutualFundB = 0;
+        double RateofReturnMutualFundC = 0;
+        double RateofReturnMutualFundD = 0;
+        double RateofReturnMutualFundE = 0;
+
+        double InitialMutualFundA = 0;
+        double InitialMutualFundB = 0;
+        double InitialMutualFundC = 0;
+        double InitialMutualFundD = 0;
+        double InitialMutualFundE = 0;
+
+        double FinalMutualFundA = 0;
+        double FinalMutualFundB = 0;
+        double FinalMutualFundC = 0;
+        double FinalMutualFundD = 0;
+        double FinalMutualFundE = 0;
+
+
+        double WeightMutualFundA = 0;
+        double WeightMutualFundB = 0;
+        double WeightMutualFundC = 0;
+        double WeightMutualFundD = 0;
+        double WeightMutualFundE = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MutualFundAInput.Text = "0";
+            MutualFundBInput.Text = "0";
+            MutualFundCInput.Text = "0";
+            MutualFundDInput.Text = "0";
+            MutualFundEInput.Text = "0";
+
+            InitialMutualFundA = 2500;
+            InitialMutualFundB = 5297;
+            InitialMutualFundC = 4115;
+            InitialMutualFundD = 5848;
+            InitialMutualFundE = 3898;
+
+            FinalMutualFundA = 3110;
+            FinalMutualFundB = 6400;
+            FinalMutualFundC = 4768;
+            FinalMutualFundD = 7559;
+            FinalMutualFundE = 4509;
+
+
+            WeightMutualFundA = 0.1164;
+            WeightMutualFundB = 0.2406;
+            WeightMutualFundC = 0.1849;
+            WeightMutualFundD = 0.2791;
+            WeightMutualFundE = 0.1791;
+
+
+
+            RateofReturnMutualFundA = (FinalMutualFundA - InitialMutualFundA) / InitialMutualFundA;
+            RateofReturnMutualFundB = (FinalMutualFundB - InitialMutualFundB) / InitialMutualFundB;
+            RateofReturnMutualFundC = (FinalMutualFundC - InitialMutualFundC) / InitialMutualFundC;
+            RateofReturnMutualFundD = (FinalMutualFundD - InitialMutualFundD) / InitialMutualFundD;
+            RateofReturnMutualFundE = (FinalMutualFundE - InitialMutualFundE) / InitialMutualFundE;
+
+
+
+
+        }
+
+        private void MutualFundA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Mutual fund A rate of return is {Math.Round(RateofReturnMutualFundA, 3) * 100 } %");
+        }
+
+        private void MutualFundB_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Mutual fund B rate of return is {Math.Round(RateofReturnMutualFundB, 3) * 100 } %");
+
+        }
+
+        private void MutualFundC_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Mutual fund C rate of return is {Math.Round(RateofReturnMutualFundC, 3) * 100 } %");
+
+        }
+
+        private void MutualFundD_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Mutual fund D rate of return is {Math.Round(RateofReturnMutualFundD, 3) * 100 } %");
+
+        }
+
+        private void MutualFundE_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Mutual fund E rate of return is {Math.Round(RateofReturnMutualFundE, 3) * 100 } %");
+
+        }
+
+        private void CustomRateofReturn_Click(object sender, EventArgs e)
+        {
+
+            WeightMutualFundA = double.Parse(MutualFundAInput.Text);
+            WeightMutualFundB = double.Parse(MutualFundBInput.Text);
+            WeightMutualFundC = double.Parse(MutualFundCInput.Text);
+            WeightMutualFundD = double.Parse(MutualFundDInput.Text);
+            WeightMutualFundE = double.Parse(MutualFundEInput.Text);
+
+            WeightMutualFundA = WeightMutualFundA / 100;
+            WeightMutualFundB = WeightMutualFundB / 100;
+            WeightMutualFundC = WeightMutualFundC / 100;
+            WeightMutualFundD = WeightMutualFundD / 100;
+            WeightMutualFundE = WeightMutualFundE / 100;
+
+
+            if ((WeightMutualFundA + WeightMutualFundB + WeightMutualFundC + WeightMutualFundD + WeightMutualFundE) == 1)
+            {
+                double PortfolioRateofReturnPercentage = (RateofReturnMutualFundA * WeightMutualFundA + RateofReturnMutualFundB * WeightMutualFundB +
+                                        RateofReturnMutualFundC * WeightMutualFundC + RateofReturnMutualFundD * WeightMutualFundD +
+                                        RateofReturnMutualFundE * WeightMutualFundE) * 100;
+
+                MessageBox.Show($"The Portfolio Rate of Return is { Math.Round(PortfolioRateofReturnPercentage, 3) } %");
+            }
+            else
+                MessageBox.Show($" Error the weightage of all mutual funds must equal 100 %");
+
+
+
+        }
+
+        private void DefaultRateofReturn_Click(object sender, EventArgs e)
+        {
+
+            WeightMutualFundA = 0.1164;
+            WeightMutualFundB = 0.2406;
+            WeightMutualFundC = 0.1849;
+            WeightMutualFundD = 0.2791;
+            WeightMutualFundE = 0.1791;
+
+
+            double PortfolioRateofReturnPercentage = (RateofReturnMutualFundA * WeightMutualFundA + RateofReturnMutualFundB * WeightMutualFundB +
+               RateofReturnMutualFundC * WeightMutualFundC + RateofReturnMutualFundD * WeightMutualFundD +
+               RateofReturnMutualFundE * WeightMutualFundE) * 100;
+
+               MessageBox.Show($"The Portfolio Rate of Return is { Math.Round(PortfolioRateofReturnPercentage, 3) } %");
+
+        }
+
+        private void MutualFundAInput_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void MutualFundBInput_TextChanged(object sender, EventArgs e)
+        {
+        
+
+        }
+
+        private void MutualFundCInput_TextChanged(object sender, EventArgs e)
+        {
+        
+
+        }
+
+        private void MutualFundDInput_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void MutualFundEInput_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
